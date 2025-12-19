6434ff1462876dd0fc44f8143a17cca11a058e40
Shivam Wayal <wayalshivam7@gmail.com>
2025-12-19 15:15:01 +0530

    Initial Spring Boot skeleton and Add actuator and Prometheus metrics

---
+++ src/main/java/io/aegisops/agent/AegisOpsApplication.java
@@ -0,0 +1,13 @@
+package io.aegisops.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisOpsApplication.class, args);
+	}
+
+}
